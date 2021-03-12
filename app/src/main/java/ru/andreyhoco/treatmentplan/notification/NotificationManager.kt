77e41dc3b0029f2a59c6cfa2c379d72f14439fef
Alexander Orlov <79202875363@ya.ru>
2021-03-12 23:36:27 +0300

    Класс NotificationManager.kt почти дописал.

--- app/src/main/java/ru/andreyhoco/treatmentplan/notification/NotificationManager.kt
+++ app/src/main/java/ru/andreyhoco/treatmentplan/notification/NotificationManager.kt
@@ -1,9 +1,17 @@
 package ru.andreyhoco.treatmentplan.notification
 
+import android.app.Notification
+import android.app.PendingIntent
+import android.content.Intent
+import androidx.annotation.MainThread
 import androidx.core.app.NotificationChannelCompat
+import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.app.NotificationManagerCompat.IMPORTANCE_DEFAULT
+import androidx.core.app.Person
 import ru.andreyhoco.treatmentplan.App
+import ru.andreyhoco.treatmentplan.presentation.ui.MainActivity
+import ru.andreyhoco.treatmentplan.repository.modelEntities.Procedure
 
 class NotificationManager() {
     private val appContext = App.appContext
@@ -18,7 +26,29 @@ class NotificationManager() {
         }
     }
 
+    fun createNotification(persons: List<Person>, procedure: List<Procedure>) : Notification {
+        return NotificationCompat.Builder(appContext, CHANNEL_NEW_PROCEDURES)
+            .setContentTitle("")
+            .setContentText("")
+            .setStyle(NotificationCompat.BigTextStyle())
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(
+                PendingIntent.getActivity(
+                    appContext,
+                    REQUEST_CONTENT,
+                    Intent(appContext, MainActivity::class.java)
+                        .setAction(Intent.ACTION_VIEW)
+                        .setData(),
+                    PendingIntent.FLAG_UPDATE_CURRENT
+                )
+            )
+            .setAutoCancel(true)
+            .build()
+    }
 
+    fun showNotification(notification: Notification) {
+        notificationManager.notify("", 1, notification)
+    }
 
     companion object {
         private const val CHANNEL_NEW_PROCEDURES = "new procedures"
