fa44058e14e849636d4ad37d4be3381155ea3cd7
Pavel Dashkevich <pavel.dashkevich@gmail.com>
2021-03-13 12:55:19 +0300

    Added conversion of list of grouped procedures to list items.

--- app/src/main/java/ru/andreyhoco/treatmentplan/presentation/ui/ProceduresListFragment.kt
+++ app/src/main/java/ru/andreyhoco/treatmentplan/presentation/ui/ProceduresListFragment.kt
@@ -5,10 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import ru.andreyhoco.treatmentplan.R
+import ru.andreyhoco.treatmentplan.presentation.viewmodels.ProceduresListViewModel
 
 class ProceduresListFragment : Fragment() {
+    private val viewModel = ProceduresListViewModel()
 
+    private lateinit var proceduresListAdapter: ProceduresListAdapter
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,5 +29,20 @@ class ProceduresListFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_procedures_list, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setupViewElements(view)
+        setupListOfProcedures()
+    }
 
+    private fun setupViewElements(view: View) {
+        recyclerView = view.findViewById(R.id.rv_procedures_list)
+    }
+
+    private fun setupListOfProcedures() {
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        proceduresListAdapter = ProceduresListAdapter(
+            viewModel.proceduresList.value as MutableList<ProcedureListItem>
+        )
+        recyclerView.adapter = proceduresListAdapter
+    }
 }
--- app/src/main/java/ru/andreyhoco/treatmentplan/presentation/viewmodels/ProceduresListViewModel.kt
+++ app/src/main/java/ru/andreyhoco/treatmentplan/presentation/viewmodels/ProceduresListViewModel.kt
@@ -1,6 +1,70 @@
 package ru.andreyhoco.treatmentplan.presentation.viewmodels
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import ru.andreyhoco.treatmentplan.presentation.ui.ProcedureGroupItem
+import ru.andreyhoco.treatmentplan.presentation.ui.ProcedureItem
+import ru.andreyhoco.treatmentplan.presentation.ui.ProcedureListItem
+import ru.andreyhoco.treatmentplan.repository.ProcedureAndPersonRepository
+import ru.andreyhoco.treatmentplan.repository.modelEntities.Procedure
+import ru.andreyhoco.treatmentplan.repository.modelEntities.ProcedureTimeGroup
+import java.util.*
 
 class ProceduresListViewModel : ViewModel() {
+    lateinit var repository: ProcedureAndPersonRepository
+
+    var proceduresList = MutableLiveData<List<ProcedureListItem>>(mutableListOf())
+
+    fun loadProceduresByDate() {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.MONTH)
+
+        calendar.set(year, month, day, 0, 0, 0)
+        calendar.set(Calendar.MILLISECOND, 0)
+
+        val startOfCurrentDay = calendar.timeInMillis
+
+        calendar.set(year, month, day, 23, 59, 59)
+        calendar.set(Calendar.MILLISECOND, 999)
+
+        val endOfCurrentDay = calendar.timeInMillis
+
+        viewModelScope.launch {
+            repository
+                .getProcedureGroupsBetweenDates(startOfCurrentDay, endOfCurrentDay)
+                .collect { listOfProcedureTimeGroup ->
+                    onProceduresLoaded(listOfProcedureTimeGroup)
+                }
+        }
+    }
+
+    private fun onProceduresLoaded(proceduresByGroup: List<ProcedureTimeGroup>) {
+        val proceduresItemsList: MutableList<ProcedureListItem> = mutableListOf()
+
+        proceduresByGroup.forEach { procedureTimeGroup ->
+            val proceduresItemsInGroup: MutableList<ProcedureItem> = mutableListOf()
+
+            procedureTimeGroup.procedures.forEach { procedure ->
+                procedure.timesOfIntake
+                    .filter { timeOfIntake ->
+                        timeOfIntake.timeOfTakes in procedureTimeGroup.startTime..procedureTimeGroup.endTime
+                    }
+                    .forEach { timeOfIntake ->
+                        proceduresItemsInGroup.add(ProcedureItem(procedure, timeOfIntake))
+                    }
+            }
+
+            if (proceduresItemsInGroup.size > 0) {
+                proceduresItemsList.add(ProcedureGroupItem(procedureTimeGroup))
+                proceduresItemsList.addAll(proceduresItemsInGroup)
+            }
+        }
+
+        proceduresList.value = proceduresItemsList
+    }
 }
