62426d16e4769381cf291e8779b5f6fb5241ec76
Alexander Orlov <79202875363@ya.ru>
2021-03-13 18:58:32 +0300

    Восстановление гита, после нажатий не туда

---
+++ app/src/main/java/ru/andreyhoco/treatmentplan/repository/modelEntities/IntakeProcedureTimeGroup.kt
@@ -0,0 +1,82 @@
+package ru.andreyhoco.treatmentplan.repository.modelEntities
+
+data class IntakeProcedureTimeGroup(
+        val startTime: Long,
+        val endTime: Long,
+        val procedures: List<IntakeProcedure>
+) {
+    constructor() : this(
+            1,
+            10,
+            listOf(
+                    IntakeProcedure(
+                            1,
+                            0,
+                            "ингаляция",
+                            Person(
+                                    1,
+                                    "Даша",
+                                    0
+                            ),
+                            "",
+                            TimeOfIntake(
+                                    System.currentTimeMillis() - 4,
+                                    true
+                            ),
+                            0,
+                            0
+                    ),
+                    IntakeProcedure(
+                            2,
+                            0,
+                            "Арбидол",
+                            Person(
+                                    1,
+                                    "Даша",
+                                    0
+                            ),
+                            "",
+                            TimeOfIntake(
+                                    System.currentTimeMillis() + 4,
+                                    true
+                            ),
+                            0,
+                            0
+                    ),
+                    IntakeProcedure(
+                            3,
+                            0,
+                            "полоскание",
+                            Person(
+                                    1,
+                                    "Даша",
+                                    0
+                            ),
+                            "",
+                            TimeOfIntake(
+                                    System.currentTimeMillis() + 1,
+                                    true
+                            ),
+                            0,
+                            0
+                    ),
+                    IntakeProcedure(
+                            4,
+                            0,
+                            "ингаляция",
+                            Person(
+                                    2,
+                                    "Вася",
+                                    0
+                            ),
+                            "",
+                            TimeOfIntake(
+                                    System.currentTimeMillis() -1,
+                                    true
+                            ),
+                            0,
+                            0
+                    )
+            )
+    )
+}
