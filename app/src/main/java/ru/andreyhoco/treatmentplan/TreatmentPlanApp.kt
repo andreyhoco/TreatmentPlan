efaee264a592a36e8ea5644c8659ca4a2331eea3
andreyhoco <andreygud2013@gmail.com>
2021-03-14 10:19:58 +0300

    Промежуточный коммит

--- app/src/main/java/ru/andreyhoco/treatmentplan/TreatmentPlanApp.kt
+++ app/src/main/java/ru/andreyhoco/treatmentplan/TreatmentPlanApp.kt
@@ -1,7 +1,20 @@
 package ru.andreyhoco.treatmentplan
 
 import android.app.Application
+import androidx.work.Configuration
 
 class TreatmentPlanApp : Application() {
     val appDi: AppDi by lazy { AppDi(applicationContext) }
+
+//    override fun getWorkManagerConfiguration(): Configuration {
+//        val myWorkerFactory = DelegatingWorkerFactory()
+//        myWorkerFactory.addFactory(ParentWorkerFactory(
+//                appDi.procedureAndPersonRepository
+//        ))
+//
+//        return Configuration.Builder()
+//                .setMinimumLoggingLevel(android.util.Log.INFO)
+//                .setWorkerFactory(myWorkerFactory)
+//                .build()
+//    }
 }
